f6c8cf19c533d1e0115297aa5cef6728ff0d2ad8
Higer <1510406760@qq.com>
2018-05-17 19:56:52 +0800

    添加相机6.0权限处理和闪光灯关闭图片

--- app/src/main/java/com/example/huanghai91632/androidbase/ui/fragment/OcrFragment.java
+++ app/src/main/java/com/example/huanghai91632/androidbase/ui/fragment/OcrFragment.java
@@ -1,15 +1,21 @@
 package com.example.huanghai91632.androidbase.ui.fragment;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.huanghai91632.androidbase.R;
 import com.example.huanghai91632.androidbase.ui.widget.ocr.ID_CameraActivity;
@@ -48,16 +54,47 @@ public class OcrFragment extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            //申请WRITE_EXTERNAL_STORAGE权限
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.CAMERA}, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        doNext(requestCode, grantResults);
+    }
+
+    private void doNext(int requestCode, int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+            } else {
+                // Permission Denied
+                //displayFrameworkBugMessageAndExit();
+                Toast.makeText(getContext(), "请在应用管理中打开“相机”访问权限!", Toast.LENGTH_LONG).show();
+                getActivity().finish();
+            }
+        }
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE && resultCode == 200) {
             String idcardNumber = "";
             IdCardInfo idCardInfo = (IdCardInfo) data.getSerializableExtra("idcardinfo");
-            if(idCardInfo != null){
+            if (idCardInfo != null) {
                 try {
-                    String res = new String(idCardInfo.getCharInfo(),"gbk");
-                    JSONObject object=new JSONObject(res);
-                    JSONObject numobject=object.getJSONObject("Num");
+                    String res = new String(idCardInfo.getCharInfo(), "gbk");
+                    JSONObject object = new JSONObject(res);
+                    JSONObject numobject = object.getJSONObject("Num");
                     idcardNumber = numobject.getString("value");
                 } catch (Exception e) {
                     e.printStackTrace();
